86c929f69057d57a08f0502fb16069aac1bbb9fa
Hanna <hannapleshko2000@mail.ru>
2022-11-29 18:58:19 +0300

    hanna commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+import app  from './src/app';
+
+config();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is running, ${port}`)
+})
--- server/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require("./src/app");
-const port = process.env.port;
-
-app.listen(port, () => {
-    console.log(`Server is running, ${port}`)
-})
